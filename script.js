d646b1e5bce859d3c3b3a1ca7fab5d0658a24582
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-12 11:51:44 +0600

    secret number (Math.random()) generated for testing in the console with proper conditions

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
 // document.querySelector('.number').textContent = 9;
 // document.querySelector('.guess-number').value = 23; // Setting input value to 23
 // console.log(document.querySelector('.guess-number').value); // Getting input value
+
+const secretNum = Math.trunc(Math.random() * 20) + 1; // Generating random number between 1 to 20
+document.querySelector('.check-btn').addEventListener('click', function () {
+  const guessedNum = Number(document.querySelector('.guess-number').value);
+  if (!guessedNum) console.log('⛔ Enter a valid Number!');
+  else if (guessedNum > secretNum) console.log('📈 Too High!');
+  else if (guessedNum < secretNum) console.log('📉 Too Low!');
+  else console.log('🎉 Yep!! You got the Number!');
+});
